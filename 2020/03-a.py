573a0480da3dc370be8f7242821ab542067affee
Aasmund Grammeltvedt <asgramme@microsoft.com>
2020-12-03 19:14:17 +0100

    Slightly less stupid abstraction

--- 2020/03-a.py
+++ 2020/03-a.py
@@ -1,7 +1,8 @@
 def tree(line, pos):
+    pos %= (len(line) - 1)
     print(line, pos, line[pos] == "#")
     return line[pos] == "#"
 
 with open("input/03.input") as f:
-    res = [tree(line, (y * 3) % (len(line) - 1)) for y, line in enumerate(f.readlines())]
+    res = [tree(line, y * 3) for y, line in enumerate(f.readlines())]
     print(sum(res))
--- 2020/03-b.py
+++ 2020/03-b.py
@@ -1,10 +1,11 @@
 def tree(line, pos):
+    pos %= (len(line) - 1)
     print(line, pos, line[pos] == "#")
     return line[pos] == "#"
 
 with open("input/03.input") as f:
     lines = f.readlines()
-    trees = [sum([tree(line, (y * xd) % (len(line) - 1)) for y, line in enumerate(lines)]) for xd in [1, 3, 5, 7]]
-    final = sum([tree(line, (y / 2) % (len(line) - 1)) for y, line in enumerate(lines) if y % 2 == 0])
+    trees = [sum([tree(line, y * xd) for y, line in enumerate(lines)]) for xd in [1, 3, 5, 7]]
+    final = sum([tree(line, y / 2) for y, line in enumerate(lines) if y % 2 == 0])
     print(trees, final)
     print(reduce(lambda x, y: x * y, trees) * final)
