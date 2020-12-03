607c0b03a7c3ca344069e17eb4abacce5a64f7ba
Aasmund Grammeltvedt <asgramme@microsoft.com>
2020-12-03 19:16:43 +0100

    Printing those \n's looks a bit silly

--- 2020/03-a.py
+++ 2020/03-a.py
@@ -1,8 +1,8 @@
 def tree(line, pos):
-    pos %= (len(line) - 1)
+    pos %= len(line) 
     print(line, pos, line[pos] == "#")
     return line[pos] == "#"
 
 with open("input/03.input") as f:
-    res = [tree(line, y * 3) for y, line in enumerate(f.readlines())]
+    res = [tree(line.strip(), y * 3) for y, line in enumerate(f.readlines())]
     print(sum(res))
--- 2020/03-b.py
+++ 2020/03-b.py
@@ -1,10 +1,10 @@
 def tree(line, pos):
-    pos %= (len(line) - 1)
+    pos %= len(line)
     print(line, pos, line[pos] == "#")
     return line[pos] == "#"
 
 with open("input/03.input") as f:
-    lines = f.readlines()
+    lines = [line.strip() for line in f.readlines()]
     trees = [sum([tree(line, y * xd) for y, line in enumerate(lines)]) for xd in [1, 3, 5, 7]]
     final = sum([tree(line, y / 2) for y, line in enumerate(lines) if y % 2 == 0])
     print(trees, final)
