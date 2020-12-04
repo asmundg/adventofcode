225ca62c51e86e7f328647b9cf3d2d20595da57c
Aasmund Grammeltvedt <asgramme@microsoft.com>
2020-12-05 00:49:15 +0100

    4th

---
+++ 2020/04-a.py
@@ -0,0 +1,11 @@
+passportKeys = sorted(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"])
+idKeys = sorted(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"])
+
+def valid(fields):
+    keys = sorted([f.split(":")[0] for f in fields.replace("\n", " ").split()])
+    print(keys)
+    return keys == passportKeys or keys == idKeys
+
+with open("input/04.input") as f:
+    data = f.read()
+    print(sum([valid(fields) for fields in data.split("\n\n")]))
