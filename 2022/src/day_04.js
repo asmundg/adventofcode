158647a3b56b99f769300c625e63fe22dbf8a7d9
Åsmund Grammeltvedt <asgramme@microsoft.com>
2022-12-04 06:26:53 +0100

    04

---
+++ 2022/src/day_04.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+
+function parse(line) {
+  return line.split(",").map((p) => p.split("-").map((c) => parseInt(c)));
+}
+
+function contains(a, b) {
+  return a[0] >= b[0] && a[1] <= b[1];
+}
+
+function overlaps(a, b) {
+  return a[0] <= b[1] && a[1] >= b[0];
+}
+
+function solve1(input) {
+  const lines = input.trim().split("\n");
+
+  count = 0;
+  for (const line of lines) {
+    const [a, b] = parse(line);
+    if (contains(a, b) || contains(b, a)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+function solve2(input) {
+  const lines = input.trim().split("\n");
+
+  count = 0;
+  for (const line of lines) {
+    const [a, b] = parse(line);
+    if (overlaps(a, b) || overlaps(b, a)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(solve1(fs.readFileSync("input/04.test", "utf8")));
+console.log(solve1(fs.readFileSync("input/04.input", "utf8")));
+
+console.log(solve2(fs.readFileSync("input/04.test", "utf8")));
+console.log(solve2(fs.readFileSync("input/04.input", "utf8")));
