d46ed6516844fc7cd49e19e8791e356c754f1431
Åsmund Grammeltvedt <asgramme@microsoft.com>
2022-12-20 16:25:11 +0100

    Clean type annotations

--- 2022/src/day_20.py
+++ 2022/src/day_20.py
@@ -12,80 +12,93 @@ cause additional cost, as compared to following the linked list.
 """
 
 import os
+from typing import List
 
 
-def parse(fname):
+class Node:
+    num: int
+    next: "Node"
+    prev: "Node"
+
+    def __init__(self, num: int):
+        self.num = num
+        self.next = None  # type: ignore
+        self.prev = None  # type: ignore
+
+
+def parse(fname: str) -> List[Node]:
     with open(fname, encoding="utf-8") as handle:
-        nums = [{"num": int(line)} for line in handle.read().strip().split("\n")]
+        nums = [Node(num=int(line)) for line in handle.read().strip().split("\n")]
         for a, b in zip(nums, nums[1:]):
-            a["next"] = b
-            b["prev"] = a
-        nums[0]["prev"] = nums[-1]
-        nums[-1]["next"] = nums[0]
+            a.next = b
+            b.prev = a
+        nums[0].prev = nums[-1]
+        nums[-1].next = nums[0]
         return nums
 
 
-def reindex(nums):
+def reindex(nums: List[Node]) -> List[Node]:
     """
     Create new list starting at 0 and ordered by link sequence
     """
     c = 0
     p = nums[0]
-    while p["num"] != 0:
+    while p.num != 0:
         if c > len(nums):
             raise Exception("Infinite loop")
-        p = p["next"]
+        p = p.next
         c += 1
 
     start = p
     indexed = []
     while True:
         indexed.append(p)
-        p = p["next"]
+        p = p.next
         if p is start:
             break
 
     return indexed
 
 
-def mix(nums):
+def mix(nums: List[Node]) -> List[Node]:
     for i, num in enumerate(nums):
-        if num["num"] == 0:
+        if num.num == 0:
             continue
 
         indexed = reindex(nums)
         pointer_index = next((i for i, pointer in enumerate(indexed) if pointer is num))
 
         # remove
-        num["next"]["prev"] = num["prev"]
-        num["prev"]["next"] = num["next"]
-        indexed = reindex(nums)
-        pointer = indexed[(pointer_index + num["num"] - 1) % len(indexed)]
+        num.next.prev = num.prev
+        num.prev.next = num.next
 
-        num["next"] = pointer["next"]
-        num["prev"] = pointer
-        num["next"]["prev"] = num
-        pointer["next"] = num
+        indexed = reindex(nums)
+        pointer = indexed[(pointer_index + num.num - 1) % len(indexed)]
+
+        num.next = pointer.next
+        num.prev = pointer
+        num.next.prev = num
+        pointer.next = num
 
         assert len(reindex(nums)) == len(nums)
 
     return nums
 
 
-def solve1(fname: str):
+def solve1(fname: str) -> int:
     nums = reindex(mix(parse(fname)))
-    return sum([nums[n % len(nums)]["num"] for n in range(1000, 3000 + 1, 1000)])
+    return sum([nums[n % len(nums)].num for n in range(1000, 3000 + 1, 1000)])
 
 
-def solve2(fname: str):
+def solve2(fname: str) -> int:
     nums = parse(fname)
     for num in nums:
-        num["num"] *= 811589153
+        num.num *= 811589153
 
     for _ in range(10):
         nums = mix(nums)
     indexed = reindex(nums)
-    return sum([indexed[n % len(nums)]["num"] for n in range(1000, 3000 + 1, 1000)])
+    return sum([indexed[n % len(nums)].num for n in range(1000, 3000 + 1, 1000)])
 
 
 day = os.path.basename(__file__).split(".")[0].split("_")[1]
