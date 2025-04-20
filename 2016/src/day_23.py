0994a6fdb9e7fa4ccbeeeec64f0c8086fbc44cd3
Åsmund Grammeltvedt <asmundg@big-oil.org>
2025-04-20 10:19:35 +0200

    2016-23

--- 2016/src/day_23.py
+++ 2016/src/day_23.py
@@ -1,8 +1,13 @@
-"""Day 12: Leonardo's Monorail
+"""Day 23: Safe Cracking
 
-This looks like a plain, minimal emulator (again - same as day 23 in
-2015)
-
+More spec implementation. This implements a multiplier of some sorts
+via repeatedly adding one. It seems like some sort of
+self-multiplication, and in theory this requires reverse engineering
+to avoid exponential scaling going out of control in part 2. However,
+the number of iterations is low enough that we can brute force it in
+completely reasonable time. I'm conditioned from later years to expect
+scaling problems to be completely unfeasible to brute force, so I'm
+now torn about accepting that brute-forcable solutions are fine.
 """
 
 import os
@@ -28,14 +33,28 @@ def parse(data: str) -> list[Instruction]:
     instructions: list[Instruction] = []
     for line in data.split("\n"):
         ins, *args = line.split()
-        instructions.append(Instruction(ins, tuple([int(arg) if arg.isdigit() else arg for arg in args])))
+        instructions.append(Instruction(ins, tuple([int(arg) if arg.lstrip("-").isdigit() else arg for arg in args])))
     return instructions
+
+
+ONE_ARGS = set(["inc", "dec", "tgl"])
+TWO_ARGS = set(["cpy", "jnz"])
+REGISTERS = "abcd"
 
 
 def exec(program: list[Instruction], registers: dict[str, int]) -> dict[str, int]:
     ptr = 0
     while ptr < len(program):
         instruction = program[ptr]
+        if instruction.ins == "tgl":
+            instruction_address = ptr + registers[str(instruction.args[0])]
+            if instruction_address < len(program):
+                target = program[instruction_address]
+                if target.ins in ONE_ARGS:
+                    program[instruction_address] = Instruction("dec" if target.ins == "inc" else "inc", target.args)
+                else:
+                    program[instruction_address] = Instruction("cpy" if target.ins == "jnz" else "jnz", target.args)
+            ptr += 1
         if instruction.ins == "cpy":
             src = instruction.args[0]
             tgt = str(instruction.args[1])
@@ -52,7 +71,8 @@ def exec(program: list[Instruction], registers: dict[str, int]) -> dict[str, int
         if instruction.ins == "jnz":
             src = instruction.args[0]
             p = src if isinstance(src, int) else registers[str(src)]
-            diff = int(instruction.args[1])
+            offset = instruction.args[1]
+            diff = offset if isinstance(offset, int) else registers[offset]
             if p != 0:
                 ptr += diff
             else:
@@ -62,23 +82,24 @@ def exec(program: list[Instruction], registers: dict[str, int]) -> dict[str, int
 
 
 def part1(program: list[Instruction]) -> int:
-    return exec(program, registers={"a": 0, "b": 0, "c": 0, "d": 0})["a"]
+    return exec(program, registers={"a": 7, "b": 0, "c": 0, "d": 0})["a"]
 
 
 def part2(program: list[Instruction]) -> int:
-    return exec(program, registers={"a": 0, "b": 0, "c": 1, "d": 0})["a"]
+    return exec(program, registers={"a": 12, "b": 0, "c": 0, "d": 0})["a"]
 
 
 def test_part1() -> None:
     data = dedent("""
-    cpy 41 a
-    inc a
-    inc a
+    cpy 2 a
+    tgl a
+    tgl a
+    tgl a
+    cpy 1 a
     dec a
-    jnz a 2
     dec a
     """).strip()
-    assert part1(parse(data)) == 42
+    assert part1(parse(data)) == 3
 
 
 if __name__ == "__main__":
